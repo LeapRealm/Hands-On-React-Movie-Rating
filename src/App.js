39b21e7aeaf34b1dc4fa81cb7911de7e99cdcde7
Jinwoo, Kim <admin@leaprealm.com>
2021-06-13 19:57:49 +0900

    영화 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "./components/Movie";
+import "./App.css"
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    // 영화 데이터 로딩!
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 6000);
-  }, []);
+  useEffect(() => getMovies(), []);
 
-  return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  const getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    setMovies(movies);
+    setIsLoading(false);
+  };
+
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader_text">"Loading..."</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            ></Movie>
+          ))}
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default App;
